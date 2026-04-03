385e41dc626243ef9ceb776ae119dfbf3c0ff8a5
hardt-fabian <fabian.hardt.it@outlook.com>
2026-04-03 14:13:24 +0200

    Initial commit

---
+++ src/main/java/com/fabianhardt/cookstock/CookstockApplication.java
@@ -0,0 +1,13 @@
+package com.fabianhardt.cookstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabianhardt/cookstock/CookstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabianhardt.cookstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
